b13cf871d2209798a8a7dd24e7869b03a6c8debd
Magnus Watn <magnus@watn.no>
2024-03-10 17:33:04 +0100

    Fix tests on Windows, maybe

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,5 +1,6 @@
 import tempfile
 from pathlib import Path
+from uuid import uuid4
 
 import pytest
 from click.testing import CliRunner
@@ -30,22 +31,23 @@ def test_no_certs_specified_gives_error_message():
 
 
 def test_clone_certs_bad_certificate_gives_error_message():
-    with tempfile.NamedTemporaryFile() as fp:
-        fp.write(b"")
-        fp.seek(0)
+    with tempfile.TemporaryDirectory() as temp_dir:
+        temp_file = Path(temp_dir).joinpath(str(uuid4))
+        temp_file.write_bytes(b"")
 
         runner = CliRunner()
-        result = runner.invoke(main, [fp.name])
+        result = runner.invoke(main, [str(temp_file)])
         assert result.exit_code == 2
         assert "Failed to parse input as certificates" in result.output
 
 
 @pytest.mark.parametrize("encoding", [Encoding.DER, Encoding.PEM])
 def test_invalid_cert_gives_error_message(dsa_cert: Certificate, encoding: Encoding):
-    with tempfile.NamedTemporaryFile() as fp:
-        fp.write(dsa_cert.public_bytes(encoding))
-        fp.seek(0)
+    with tempfile.TemporaryDirectory() as temp_dir:
+        temp_file = Path(temp_dir).joinpath(str(uuid4))
+        temp_file.write_bytes(dsa_cert.public_bytes(encoding))
+
         runner = CliRunner()
-        result = runner.invoke(main, [fp.name])
+        result = runner.invoke(main, [str(temp_file)])
         assert result.exit_code == 2
         assert " Unsupported key in cert CN=DSA key: DSAPublicKey" in result.output
