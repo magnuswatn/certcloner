4a6b9f2c235b50fc6a0eddbae1b736e980c5f648
Magnus Watn <magnus@watn.no>
2024-03-10 17:36:35 +0100

    Call the function...

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -32,7 +32,7 @@ def test_no_certs_specified_gives_error_message():
 
 def test_clone_certs_bad_certificate_gives_error_message():
     with tempfile.TemporaryDirectory() as temp_dir:
-        temp_file = Path(temp_dir).joinpath(str(uuid4))
+        temp_file = Path(temp_dir).joinpath(str(uuid4()))
         temp_file.write_bytes(b"")
 
         runner = CliRunner()
@@ -44,7 +44,7 @@ def test_clone_certs_bad_certificate_gives_error_message():
 @pytest.mark.parametrize("encoding", [Encoding.DER, Encoding.PEM])
 def test_invalid_cert_gives_error_message(dsa_cert: Certificate, encoding: Encoding):
     with tempfile.TemporaryDirectory() as temp_dir:
-        temp_file = Path(temp_dir).joinpath(str(uuid4))
+        temp_file = Path(temp_dir).joinpath(str(uuid4()))
         temp_file.write_bytes(dsa_cert.public_bytes(encoding))
 
         runner = CliRunner()
